c2065e35bce1dfdcf9b0ae7df3a4b8e09cba899d
Serhii Tyron <mestn1y@hotmail.com>
2024-12-01 17:40:44 +0200

    add some props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,26 @@ import "./App.css";
 export const App = () => {
   return (
     <div>
-      <h1>Products</h1>
-      <Product />
-      <Product />
+      <h1>Best selling</h1>
+      <Product
+        name="Tacos with lime"
+        imageURL="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and burger"
+        imageURL="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
       <Product />
     </div>
   );
 };
 
+// components
 // Ім'я компонента обов'язково повинно починатися з великої літери.
 // Назви компонентів з маленької літери зарезервовані для HTML - елементів.
 // Якщо ви спробуєте назвати компонент product, а не Product, під час рендеру React проігнорує його та відрендерить тег < product ></ >.
+
+// props
+// Пропси використовуються для передачі динамічних значень для компонента, наприклад, для використання в JSX-розмітці, використовуючи синтаксис {}.
